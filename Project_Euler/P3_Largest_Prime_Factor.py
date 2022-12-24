6b108c065a75d52086afce3e8944a990c7269cae
p3rpl3x1ty <p3rpl3x1ty1@gmail.com>
2022-12-24 00:14:46 -0600

    Create P3_Largest_Prime_Factor.py

---
+++ Project_Euler/P3_Largest_Prime_Factor.py
@@ -0,0 +1,14 @@
+def largest_prime_factor(n):
+    # Initialize the largest prime factor to be 1
+    largest_prime_factor = 1
+    
+    # Start from 2, and check if each number is a factor of n
+    for i in range(2, n+1):
+        # If i is a factor of n, divide n by i until it is not divisible by i anymore
+        while n % i == 0:
+            # Update the value of n and the largest prime factor
+            n = n / i
+            largest_prime_factor = i
+    
+    # Return the largest prime factor
+    return largest_prime_factor
