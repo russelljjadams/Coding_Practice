0a9eec48508c65317dedf500c491fd0acd852553
p3rpl3x1ty <p3rpl3x1ty1@gmail.com>
2022-12-25 11:05:36 -0600

    Create P4_Largest_Palindrome_Product.py

---
+++ Project_Euler/P4_Largest_Palindrome_Product.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+l = 0
+for i in range(100,1000):
+    for x in range(100,1000):
+        r = i*x
+        if str(r) == str(r)[::-1]:
+            if r > l:
+                l = r
+print(l)
